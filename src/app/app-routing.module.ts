97b3affeb7dba04770199e6eaf28bd7f737a59e4
sholom <sholomls@gmail.com>
2019-06-02 14:41:21 -0400

    added search and detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MoviesComponent} from './views/results/movies/movies.component';
+import {MovieDetailsComponent} from './views/results/movies/movie-details/movie-details.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'results'},
-  {path: 'results', component: MoviesComponent}
+  {path: 'results', component: MoviesComponent},
+  {path: 'results:id', component: MovieDetailsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './views/results/movies/movies.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MovieDetailsComponent } from './views/results/movies/movie-details/movie-details.component';
+import { SearchComponent } from './views/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieDetailsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
